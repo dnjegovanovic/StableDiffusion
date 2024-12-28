8b16ee797789fbafd417b4ec7e6fb64a052b4942
rudnik-zag <rudnikzagradje@gmail.com>
2024-12-28 22:53:58 +0100

    Implementing VAE.

---
+++ models/decoder.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from attention import SelfAttention
+
+class VAEResiualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # TODO: Implemet residual block
+
+class VAEAttentionBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        # TODO: Implemet attention block
