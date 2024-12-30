5cc5382e9e1412382761fcb8c70d9a200a7e20ce
rudnik-zag <rudnikzagradje@gmail.com>
2024-12-30 10:12:50 +0100

    Build CLIP

--- models/attention.py
+++ models/attention.py
@@ -3,8 +3,30 @@ import torch.nn as nn
 from torch.nn import functional as F
 import math
 
+
 class SelfAttention(nn.Module):
-    def __init__(self, n_heads, d_embed, in_proj_bias=True, out_proj_bias=True):
+    def __init__(
+        self,
+        n_heads: int,
+        d_embed: int,
+        in_proj_bias: bool = True,
+        out_proj_bias: bool = True,
+    ):
+        """
+        Initialize the SelfAttention module.
+
+        Args:
+            n_heads (int): The number of attention heads.
+            d_embed (int): The dimension of the input and output embeddings.
+            in_proj_bias (bool, optional): Whether to include bias in the input projection layer. Defaults to True.
+            out_proj_bias (bool, optional): Whether to include bias in the output projection layer. Defaults to True.
+
+        Attributes:
+            in_proj (nn.Linear): The linear layer that combines the Wq, Wk, and Wv matrices into one matrix.
+            out_proj (nn.Linear): The linear layer that represents the Wo matrix.
+            n_heads (int): The number of attention heads.
+            d_head (int): The dimension of each attention head.
+        """
         super().__init__()
         # This combines the Wq, Wk and Wv matrices into one matrix
         self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
@@ -14,20 +36,30 @@ class SelfAttention(nn.Module):
         self.d_head = d_embed // n_heads
 
     def forward(self, x, causal_mask=False):
+        """
+        Perform the forward pass of the self-attention mechanism.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (Batch_Size, Seq_Len, Dim).
+            causal_mask (bool, optional): If True, applies a causal mask to prevent attending to future positions. Defaults to False.
+
+        Returns:
+            torch.Tensor: Output tensor of shape (Batch_Size, Seq_Len, Dim) after applying self-attention.
+        """
         # x: # (Batch_Size, Seq_Len, Dim)
 
         # (Batch_Size, Seq_Len, Dim)
-        input_shape = x.shape 
-        
+        input_shape = x.shape
+
         # (Batch_Size, Seq_Len, Dim)
-        batch_size, sequence_length, d_embed = input_shape 
+        batch_size, sequence_length, d_embed = input_shape
 
         # (Batch_Size, Seq_Len, H, Dim / H)
-        interim_shape = (batch_size, sequence_length, self.n_heads, self.d_head) 
+        interim_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
 
         # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim * 3) -> 3 tensor of shape (Batch_Size, Seq_Len, Dim)
         q, k, v = self.in_proj(x).chunk(3, dim=-1)
-        
+
         # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, H, Dim / H) -> (Batch_Size, H, Seq_Len, Dim / H)
         q = q.view(interim_shape).transpose(1, 2)
         k = k.view(interim_shape).transpose(1, 2)
@@ -35,31 +67,89 @@ class SelfAttention(nn.Module):
 
         # (Batch_Size, H, Seq_Len, Dim / H) @ (Batch_Size, H, Dim / H, Seq_Len) -> (Batch_Size, H, Seq_Len, Seq_Len)
         weight = q @ k.transpose(-1, -2)
-        
+
         if causal_mask:
             # Mask where the upper triangle (above the principal diagonal) is 1
-            mask = torch.ones_like(weight, dtype=torch.bool).triu(1) 
+            mask = torch.ones_like(weight, dtype=torch.bool).triu(1)
             # Fill the upper triangle with -inf
-            weight.masked_fill_(mask, -torch.inf) 
-        
-        # Divide by d_k (Dim / H). 
+            weight.masked_fill_(mask, -torch.inf)
+
+        # Divide by d_k (Dim / H).
         # (Batch_Size, H, Seq_Len, Seq_Len) -> (Batch_Size, H, Seq_Len, Seq_Len)
-        weight /= math.sqrt(self.d_head) 
+        weight /= math.sqrt(self.d_head)
 
         # (Batch_Size, H, Seq_Len, Seq_Len) -> (Batch_Size, H, Seq_Len, Seq_Len)
-        weight = F.softmax(weight, dim=-1) 
+        weight = F.softmax(weight, dim=-1)
 
         # (Batch_Size, H, Seq_Len, Seq_Len) @ (Batch_Size, H, Seq_Len, Dim / H) -> (Batch_Size, H, Seq_Len, Dim / H)
         output = weight @ v
 
         # (Batch_Size, H, Seq_Len, Dim / H) -> (Batch_Size, Seq_Len, H, Dim / H)
-        output = output.transpose(1, 2) 
+        output = output.transpose(1, 2)
 
         # (Batch_Size, Seq_Len, H, Dim / H) -> (Batch_Size, Seq_Len, Dim)
-        output = output.reshape(input_shape) 
+        output = output.reshape(input_shape)
 
         # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
-        output = self.out_proj(output) 
-        
+        output = self.out_proj(output)
+
         # (Batch_Size, Seq_Len, Dim)
+        return output
+
+
+class CrossAttention(nn.Module):
+    def __init__(self, n_heads, d_embed, d_cross, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+        self.q_proj   = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+        self.k_proj   = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.v_proj   = nn.Linear(d_cross, d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+    
+    def forward(self, x, y):
+        # x (latent): # (Batch_Size, Seq_Len_Q, Dim_Q)
+        # y (context): # (Batch_Size, Seq_Len_KV, Dim_KV) = (Batch_Size, 77, 768)
+
+        input_shape = x.shape
+        batch_size, sequence_length, d_embed = input_shape
+        # Divide each embedding of Q into multiple heads such that d_heads * n_heads = Dim_Q
+        interim_shape = (batch_size, -1, self.n_heads, self.d_head)
+        
+        # (Batch_Size, Seq_Len_Q, Dim_Q) -> (Batch_Size, Seq_Len_Q, Dim_Q)
+        q = self.q_proj(x)
+        # (Batch_Size, Seq_Len_KV, Dim_KV) -> (Batch_Size, Seq_Len_KV, Dim_Q)
+        k = self.k_proj(y)
+        # (Batch_Size, Seq_Len_KV, Dim_KV) -> (Batch_Size, Seq_Len_KV, Dim_Q)
+        v = self.v_proj(y)
+
+        # (Batch_Size, Seq_Len_Q, Dim_Q) -> (Batch_Size, Seq_Len_Q, H, Dim_Q / H) -> (Batch_Size, H, Seq_Len_Q, Dim_Q / H)
+        q = q.view(interim_shape).transpose(1, 2) 
+        # (Batch_Size, Seq_Len_KV, Dim_Q) -> (Batch_Size, Seq_Len_KV, H, Dim_Q / H) -> (Batch_Size, H, Seq_Len_KV, Dim_Q / H)
+        k = k.view(interim_shape).transpose(1, 2) 
+        # (Batch_Size, Seq_Len_KV, Dim_Q) -> (Batch_Size, Seq_Len_KV, H, Dim_Q / H) -> (Batch_Size, H, Seq_Len_KV, Dim_Q / H)
+        v = v.view(interim_shape).transpose(1, 2) 
+        
+        # (Batch_Size, H, Seq_Len_Q, Dim_Q / H) @ (Batch_Size, H, Dim_Q / H, Seq_Len_KV) -> (Batch_Size, H, Seq_Len_Q, Seq_Len_KV)
+        weight = q @ k.transpose(-1, -2)
+        
+        # (Batch_Size, H, Seq_Len_Q, Seq_Len_KV)
+        weight /= math.sqrt(self.d_head)
+        
+        # (Batch_Size, H, Seq_Len_Q, Seq_Len_KV)
+        weight = F.softmax(weight, dim=-1)
+        
+        # (Batch_Size, H, Seq_Len_Q, Seq_Len_KV) @ (Batch_Size, H, Seq_Len_KV, Dim_Q / H) -> (Batch_Size, H, Seq_Len_Q, Dim_Q / H)
+        output = weight @ v
+        
+        # (Batch_Size, H, Seq_Len_Q, Dim_Q / H) -> (Batch_Size, Seq_Len_Q, H, Dim_Q / H)
+        output = output.transpose(1, 2).contiguous()
+        
+        # (Batch_Size, Seq_Len_Q, H, Dim_Q / H) -> (Batch_Size, Seq_Len_Q, Dim_Q)
+        output = output.view(input_shape)
+        
+        # (Batch_Size, Seq_Len_Q, Dim_Q) -> (Batch_Size, Seq_Len_Q, Dim_Q)
+        output = self.out_proj(output)
+
+        # (Batch_Size, Seq_Len_Q, Dim_Q)
         return output
--- models/decoder.py
+++ models/decoder.py
@@ -112,87 +112,62 @@ class VAEDecoder(nn.Sequential):
         super().__init__(
             # (Batch_Size, 4, Height / 8, Width / 8) -> (Batch_Size, 4, Height / 8, Width / 8)
             nn.Conv2d(4, 4, kernel_size=1, padding=0),
-
             # (Batch_Size, 4, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 8, Width / 8)
             nn.Conv2d(4, 512, kernel_size=3, padding=1),
-            
             # (Batch_Size, 512, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 8, Width / 8)
-            VAEResidualBlock(512, 512), 
-            
+            VAEResidualBlock(512, 512),
             # (Batch_Size, 512, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 8, Width / 8)
-            VAEAttentionBlock(512), 
-            
+            VAEAttentionBlock(512),
             # (Batch_Size, 512, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 8, Width / 8)
-            VAEResidualBlock(512, 512), 
-            
+            VAEResidualBlock(512, 512),
             # (Batch_Size, 512, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 8, Width / 8)
-            VAEResidualBlock(512, 512), 
-            
+            VAEResidualBlock(512, 512),
             # (Batch_Size, 512, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 8, Width / 8)
-            VAEResidualBlock(512, 512), 
-            
+            VAEResidualBlock(512, 512),
             # (Batch_Size, 512, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 8, Width / 8)
-            VAEResidualBlock(512, 512), 
-            
+            VAEResidualBlock(512, 512),
             # Repeats the rows and columns of the data by scale_factor (like when you resize an image by doubling its size).
             # (Batch_Size, 512, Height / 8, Width / 8) -> (Batch_Size, 512, Height / 4, Width / 4)
             nn.Upsample(scale_factor=2),
-            
             # (Batch_Size, 512, Height / 4, Width / 4) -> (Batch_Size, 512, Height / 4, Width / 4)
-            nn.Conv2d(512, 512, kernel_size=3, padding=1), 
-            
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
             # (Batch_Size, 512, Height / 4, Width / 4) -> (Batch_Size, 512, Height / 4, Width / 4)
-            VAEResidualBlock(512, 512), 
-            
+            VAEResidualBlock(512, 512),
             # (Batch_Size, 512, Height / 4, Width / 4) -> (Batch_Size, 512, Height / 4, Width / 4)
-            VAEResidualBlock(512, 512), 
-            
+            VAEResidualBlock(512, 512),
             # (Batch_Size, 512, Height / 4, Width / 4) -> (Batch_Size, 512, Height / 4, Width / 4)
-            VAEResidualBlock(512, 512), 
-            
+            VAEResidualBlock(512, 512),
             # (Batch_Size, 512, Height / 4, Width / 4) -> (Batch_Size, 512, Height / 2, Width / 2)
-            nn.Upsample(scale_factor=2), 
-            
+            nn.Upsample(scale_factor=2),
             # (Batch_Size, 512, Height / 2, Width / 2) -> (Batch_Size, 512, Height / 2, Width / 2)
-            nn.Conv2d(512, 512, kernel_size=3, padding=1), 
-            
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
             # (Batch_Size, 512, Height / 2, Width / 2) -> (Batch_Size, 256, Height / 2, Width / 2)
-            VAEResidualBlock(512, 256), 
-            
+            VAEResidualBlock(512, 256),
             # (Batch_Size, 256, Height / 2, Width / 2) -> (Batch_Size, 256, Height / 2, Width / 2)
-            VAEResidualBlock(256, 256), 
-            
+            VAEResidualBlock(256, 256),
             # (Batch_Size, 256, Height / 2, Width / 2) -> (Batch_Size, 256, Height / 2, Width / 2)
-            VAEResidualBlock(256, 256), 
-            
+            VAEResidualBlock(256, 256),
             # (Batch_Size, 256, Height / 2, Width / 2) -> (Batch_Size, 256, Height, Width)
-            nn.Upsample(scale_factor=2), 
-            
+            nn.Upsample(scale_factor=2),
             # (Batch_Size, 256, Height, Width) -> (Batch_Size, 256, Height, Width)
-            nn.Conv2d(256, 256, kernel_size=3, padding=1), 
-            
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
             # (Batch_Size, 256, Height, Width) -> (Batch_Size, 128, Height, Width)
-            VAEResidualBlock(256, 128), 
-            
+            VAEResidualBlock(256, 128),
             # (Batch_Size, 128, Height, Width) -> (Batch_Size, 128, Height, Width)
-            VAEResidualBlock(128, 128), 
-            
+            VAEResidualBlock(128, 128),
             # (Batch_Size, 128, Height, Width) -> (Batch_Size, 128, Height, Width)
-            VAEResidualBlock(128, 128), 
-            
+            VAEResidualBlock(128, 128),
             # (Batch_Size, 128, Height, Width) -> (Batch_Size, 128, Height, Width)
-            nn.GroupNorm(32, 128), 
-            
+            nn.GroupNorm(32, 128),
             # (Batch_Size, 128, Height, Width) -> (Batch_Size, 128, Height, Width)
-            nn.SiLU(), 
-            
+            nn.SiLU(),
             # (Batch_Size, 128, Height, Width) -> (Batch_Size, 3, Height, Width)
-            nn.Conv2d(128, 3, kernel_size=3, padding=1), 
+            nn.Conv2d(128, 3, kernel_size=3, padding=1),
         )
 
     def forward(self, x):
         # x: (Batch_Size, 4, Height / 8, Width / 8)
-        
+
         # Remove the scaling added by the Encoder.
         x /= 0.18215
 
